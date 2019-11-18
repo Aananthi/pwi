69444fa21f4bdc5abd8ca200522cf4bc2fba9783
Aananthi.J.S <aananthi25032001@gmail.com>
2019-11-18 10:29:11 +0530

    initial commit

---
+++ app/src/main/java/com/example/parentweb/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.parentweb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
